674c4a7ea4c2adff3d47e4aabdd0c039a67944ce
215708 <215708@upf.br>
2026-06-12 22:42:38 -0300

    White theme

--- JS/script.js
+++ JS/script.js
@@ -10,3 +10,11 @@ function fontDown() {
 //     document.body.classList.remove('bigfont');
 //     document.body.classList.remove('smallfont');
 // }
+
+let lightTheme = document.getElementById("lightTheme");
+let darkTheme = document.getElementById("darkTheme");
+
+function toggleTheme() {
+    lightTheme.disabled = !lightTheme.disabled;
+    darkTheme.disabled = !darkTheme.disabled;
+}

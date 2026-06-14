33eea64abd913fd2bd780b750b48d2adde70efc3
215708 <215708@upf.br>
2026-06-14 10:34:17 -0300

    Finalizado

--- JS/script.js
+++ JS/script.js
@@ -18,3 +18,28 @@ function toggleTheme() {
     lightTheme.disabled = !lightTheme.disabled;
     darkTheme.disabled = !darkTheme.disabled;
 }
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let nome = document.querySelector('input[type="text"]');
+    let email = document.querySelector('input[type="email"]');
+    let telefone = document.querySelector('input[type="tel"]');
+
+    if (nome.value.trim().length <= 4) {
+        alert("ERRO: PRECISA ser o nome completo, eu duvido que seu nome seja tão curto.");
+        return;
+    }
+
+    if (!email.value.includes("@")) {
+        alert("ERRO: o email deve conter @");
+        return;
+    }
+
+    if (/[a-zA-Z]/.test(telefone.value)) {
+        alert("ERRO: O telefone não pode conter letras.");
+        return;
+    }
+
+    alert("Mensagem enviada!");
+});
---
+++ README.txt
@@ -0,0 +1,1 @@
+https://eduardotramos.github.io/index.html
